1b31573dc5325ffe89c2ee3215331298f5357494
olivera_julieta@outlook.com <olivera_julieta@outlook.com>
2022-09-08 20:45:34 -0300

    Agregando holis.txt

---
+++ saludar.py
@@ -0,0 +1,22 @@
+import sys 
+import os 
+
+# ~ nombre = sys.argv[1]
+# ~ print("Bienvenid@ al sistema " + nombre)
+
+#Tratar el error
+# ~ try:
+	# ~ nombre = sys.argv[1]
+# ~ except IndexError:
+	# ~ print("Debes pasar un nombre")
+# ~ else:
+	# ~ print("Bienvenid@ la sistema " + nombre)
+
+# con varios nombres
+# ~ nombres = sys.argv[1:]
+# ~ #Chequeamos que la lista no este vacia
+# ~ if nombres:
+	# ~ for nombre in nombres:
+		# ~ print("Hola " + nombre)
+# ~ else:
+	# ~ print("Debes ingresar un nombre")
